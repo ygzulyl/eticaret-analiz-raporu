d6bb0987d0a1cf81e70e9827bc84c1e894e60b2b
ygzulyl <24litt012@istanbulticaret.edu.tr>
2026-06-08 05:48:42 +0300

    Delete bilgisayar.py

--- bilgisayar.py
+++
@@ -1,7 +0,0 @@
-def bilgisayar_topla(marka, *ozellikler):
-    print(f"Aşağıdaki özelliklerle {marka} bilgisayar oluşturuldu")
-    for ozellik in ozellikler:
-        print(ozellik)
-
-def bilgisayar_sil(marka):
-    print(f"{marka} bilgisayar silindi")
